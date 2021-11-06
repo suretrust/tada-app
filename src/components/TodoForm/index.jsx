40d5479de47b1d2aa4a3619a8752d355ab0599b6
Saheed Oladele <sdrfngg640@gmail.com>
2021-11-06 10:06:48 +0100

    feat: add TodoForm component

---
+++ src/components/TodoForm/index.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './style.css'
+
+const TodoForm = ({
+  handleSubmit,
+  hideForm,
+  setFormState,
+  formState,
+  buttonText
+}) => {
+  const handleChange = e => {
+    const { name, type, checked, value: eventValue } = e.target
+    const value = type === 'checkbox' ? checked : eventValue.slice(0, 110)
+
+    setFormState(fields => ({ ...fields, [name]: value }))
+  }
+
+  return (
+    <div className='overlay'>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={formState.title}
+          type='text'
+          placeholder='Enter todo title'
+          name='title'
+          id='title'
+          onChange={handleChange}
+          required
+        />
+        <textarea
+          value={formState.description}
+          placeholder='Enter todo description &#10;(110 characters max)'
+          type='text'
+          name='description'
+          onChange={handleChange}
+          id='description'
+          required
+        />
+        <label htmlFor='completed' className='status'>
+          <input
+            checked={formState.completed}
+            type='checkbox'
+            onChange={handleChange}
+            id='completed'
+            name='completed'
+            placeholder='Completed'
+          />
+          <span className='check-mark' />
+          <span className='label-text'>Completed</span>
+        </label>
+        <div className='buttons-box'>
+          <button onClick={hideForm} type='button'>
+            Cancel
+          </button>
+          <button type='submit'>{buttonText}</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+TodoForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  hideForm: PropTypes.func.isRequired,
+  setFormState: PropTypes.func.isRequired,
+  buttonText: PropTypes.string.isRequired,
+  formState: PropTypes.shape({
+    completed: PropTypes.bool.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired
+  }).isRequired
+}
+
+export default TodoForm
