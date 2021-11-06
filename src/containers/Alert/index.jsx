9eb54ffa2087d612cf12579da28763a1b8abb84f
Saheed Oladele <sdrfngg640@gmail.com>
2021-11-06 10:07:26 +0100

    feat: add Alert component

---
+++ src/containers/Alert/index.jsx
@@ -0,0 +1,40 @@
+import React, { useCallback, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { X } from 'react-feather'
+
+import './style.css'
+import { deleteAlertText } from '../../redux/actions/alert/deleteAlert'
+
+const Alert = () => {
+  const { text } = useSelector(state => state.todos)
+  const dispatch = useDispatch()
+
+  const clearAlert = useCallback(() => {
+    dispatch(deleteAlertText())
+  }, [dispatch])
+
+  useEffect(() => {
+    const timer = setTimeout(() => clearAlert(), 3000)
+
+    return () => clearTimeout(timer)
+  }, [text, clearAlert])
+
+  if (!text?.length) return null
+
+  return (
+    <div className='alert'>
+      {text}
+      <span className='icon' onClick={clearAlert}>
+        <X />
+      </span>
+    </div>
+  )
+}
+
+Alert.propTypes = {
+  text: PropTypes.string.isRequired
+}
+
+export default Alert
